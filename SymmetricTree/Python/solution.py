6755b6a4b1cba1da052578f01fc772cff1da6eb8
Casey McGuire <caseymcguire90@gmail.com>
2014-12-20 14:04:10 -0800

    Added path to testing

--- SymmetricTree/Python/solution.py
+++ SymmetricTree/Python/solution.py
@@ -5,8 +5,18 @@
 #        self.left = None
 #        self.right = None
 #
+import sys
+sys.path.insert(0, '/home/casey/Desktop/CodingProblems/leetcode/Testing/Python')
+import TreeNode
 
 class Solution:
     # @param root, a tree node
     # @return a boolean
     def isSymmetric(self, root):
+        if not root:
+            return True
+
+
+
+if __name__ == '__main__':
+    print "Hello World"
