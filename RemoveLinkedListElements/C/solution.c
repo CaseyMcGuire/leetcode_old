0a9df8e3368849d10336c6025d51a7a0f9e7de33
Adam DeRusha <apd3691@gmail.com>
2015-05-18 17:56:33 -0700

    Fixed the input not being updated.

--- RemoveLinkedListElements/C/solution.c
+++ RemoveLinkedListElements/C/solution.c
@@ -28,8 +28,9 @@ struct ListNode* removeElements(struct ListNode* head, int val){
   return head;
 }
 
-void run_test(char *test_name, struct ListNode *input, struct ListNode *expected_result, int element_to_remove){
-  struct ListNode *result = removeElements(input, element_to_remove);
+void run_test(char *test_name, struct ListNode **input, struct ListNode *expected_result, int element_to_remove){
+  struct ListNode *result = removeElements(*input, element_to_remove);
+  *input = result;
   int passed = lists_equal(result, expected_result);
   printf("%s", test_name);
   if(passed == 1){
@@ -50,17 +51,17 @@ int main(int argc, char *argv[]){
   struct ListNode *input = NULL;
   struct ListNode *expected_result = NULL;
   
-  run_test("Null test", input, expected_result, 0);
+  run_test("Null test", &input, expected_result, 0);
 
   //remove value from list with only one element
   int arr1[] = {1};
   input = init_list(arr1, 1);
-  run_test("Remove single element test", input, NULL, 1);
+  run_test("Remove single element test", &input, NULL, 1);
   free_list(input);
 
   //don't remove value from single list
   input = init_list(arr1, 1);
-  run_test("Don't remove single element test", input, input, 0);
+  run_test("Don't remove single element test", &input, input, 0);
   free_list(input);
 
   //remove multiple head values
@@ -68,7 +69,7 @@ int main(int argc, char *argv[]){
   input = init_list(arr2, 8);
   int result_arr2[] = {2};
   expected_result = init_list(result_arr2, 1);
-  run_test("Remove multiple heads test", input, expected_result, 1);
+  run_test("Remove multiple heads test", &input, expected_result, 1);
   free_list(input);
   print_list(expected_result);
   free_list(expected_result);
@@ -77,7 +78,7 @@ int main(int argc, char *argv[]){
   input = init_list(arr3, 6);
   int result_arr3[] = {1,2,3,4,5};
   expected_result = init_list(result_arr3, 5);
-  run_test("Remove tail from list", input, expected_result, 6);
+  run_test("Remove tail from list", &input, expected_result, 6);
   free_list(input);
   free_list(expected_result);
 
@@ -85,7 +86,7 @@ int main(int argc, char *argv[]){
   input = init_list(arr4, 7);
   int result_arr4[] = {1,2,3,4,5};
   expected_result = init_list(result_arr4, 5);
-  run_test("Remove multiple elements at tail of list", input, expected_result, 6);
+  run_test("Remove multiple elements at tail of list", &input, expected_result, 6);
   free_list(input);
   free_list(expected_result);
 
@@ -93,7 +94,7 @@ int main(int argc, char *argv[]){
   input = init_list(arr5, 6);
   int result_arr5[] = {1,2,3,5,6};
   expected_result = init_list(result_arr5, 5);
-  run_test("Remove element from middle of list", input, expected_result, 4);
+  run_test("Remove element from middle of list", &input, expected_result, 4);
   free_list(input);
   free_list(expected_result);
 }
