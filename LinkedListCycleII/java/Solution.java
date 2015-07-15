26b66bd1e1a69ce19959193c19c5f853ea4ba03c
Casey McGuire <casey.jay.mcguire@gmail.com>
2015-07-14 20:03:20 -0700

    Finished two more solutions

--- LinkedListCycleII/java/Solution.java
+++ LinkedListCycleII/java/Solution.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Solution {
 
     //O(n) time and O(1) space but basically destroys the list and apparently can't be graded
@@ -22,6 +23,37 @@ public class Solution {
 	    if((fastNode = fastNode.next) == null) return false;
 	    slowNode = slowNode.next;
 	}
+    }
+
+    //O(n) time, O(n) space
+    public static ListNode detectCycle2(ListNode head) {
+	Set<ListNode> visitedNodes = new HashSet<>();
+	ListNode curNode = head;
+	while(true) {
+	    if(curNode == null) return null;
+	    if(visitedNodes.contains(curNode)) return curNode;
+	    visitedNodes.add(curNode);
+	    curNode = curNode.next;
+	}
+    }
+
+    public static ListNode detectCycle3(ListNode head) {
+	if(head == null) return null;
+	ListNode fastNode = head;
+	ListNode slowNode = head;
+	for(;;) {
+	    if((fastNode = fastNode.next) == null) return null;
+	    if((fastNode = fastNode.next) == null) return null;
+	    slowNode = slowNode.next;
+	    if(fastNode == slowNode) break;
+	}
+
+	slowNode = head;
+	while(slowNode != fastNode) {
+	    slowNode = slowNode.next;
+	    fastNode = fastNode.next;
+	}
+	return slowNode;
     }
 
     public static ListNode getNth(ListNode head, int nth) {
