03f2756adeb8f25e5e3132b9341259929993e9d3
Casey McGuire <casey.jay.mcguire@gmail.com>
2015-09-06 14:10:08 -0700

    Finished better solution in C

--- IntegerToRoman/c/solution.c
+++ IntegerToRoman/c/solution.c
@@ -1,100 +1,60 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+//                  1000, 500, 100, 50, 10, 5, 1
+char mappings[] = {'M', 'D', 'C', 'L', 'X', 'V', 'I' };
+int places[] = {1000,100,10,1};
+
+void fill_up(int cur_place, int mapping_index, int* str_index, char* str, int* cur_total);
 
 char* intToRoman(int num) {
-  char* str = malloc(30 * sizeof(char));
-  int i;
+  
+  //Largest roman numeral possible is 3888 (MMMDCCCLXXXVIII)
+  char* str = malloc(16 * sizeof(char));
+  int i, str_index = 0, mapping_index = 0;
   for(i = 0; num != 0; i++) {
-    get_next_numerals(str, &i, &num);
+    fill_up(i, mapping_index, &str_index, str, &num);
+    mapping_index += 2;
   }
-  str[i] = '\0';
+  str[str_index] = '\0';
   return str;
 }
 
-get_next_numerals(char* str, int* index, int* total) {
-  if(*total >= 1000) {
-    str[*index] = 'M';
-    *total -= 1000;
-  }
-  else if(*total >= 900) {
-    str[*index] = 'C';
-    (*index)++;
-    str[*index] = 'M';
-    *total -= 900;
-  }
-  else if(*total >= 500) {
-    str[*index] = 'D';
-    while(*total >= 600) {
-      (*index)++;
-      str[*index] = 'C';
-      *total -= 100;
-    }
-    *total -= 500;
-  }
-  else if(*total >= 400) {
-    str[*index] = 'C';
-    (*index)++;
-    str[*index] = 'D';
-    *total -= 400;
-  }
-  else if(*total >= 100) {
-    str[*index] = 'C';
-    *total -= 100;
-  }
-  else if(*total >= 90) {
-    str[*index] = 'X';
-    (*index)++;
-    str[*index] = 'C';
-    *total -= 90;
-  }
-  else if(*total >= 50) {
-    str[*index] = 'L';
-    while(*total >= 60) {
-      (*index)++;
-      str[*index] = 'X';
-      *total -= 10;
-    }
-    *total -= 50;
-  }
-  else if(*total >= 40) {
-    str[*index] = 'X';
-    (*index)++;
-    str[*index] = 'L';
-    *total -= 40;
-  }
-  else if(*total >= 10) {
-    str[*index] = 'X';
-    *total -= 10;
-  }
-  else if(*total == 9) {
-    str[*index] = 'I';
-    (*index)++;
-    str[*index] = 'X';
-    *total -= 9;
-  }
-  else if(*total >= 5) {
-    str[*index] = 'V';
-    while(*total >= 6) {
-      (*index)++;
-      str[*index] = 'I';
-      *total -= 1;
-    }
-    *total -= 5;
-  }
-  else if(*total == 4) {
-    str[*index] = 'I';
-    (*index)++;
-    str[*index] = 'V';
-    *total -= 4;
-  }
-  else {
-    str[*index] = 'I';
-    (*total)--;
-  }
+void fill_up(int cur_place, int mapping_index, int* str_index, char* str, int* cur_total) {
+  int num_times_into = (*cur_total)/places[cur_place];
 
-
+  if(num_times_into >= 1 && num_times_into < 4) {
+    int i;
+    for(i = 0; i < num_times_into; i++) {
+      str[*str_index] = mappings[mapping_index];
+      (*str_index)++;
+    }
+  }
+  else if(num_times_into == 4) {
+    str[*str_index] = mappings[mapping_index];
+    (*str_index)++;
+    str[*str_index] = mappings[mapping_index-1];
+    (*str_index)++;
+  }
+  else if(num_times_into >= 5 && num_times_into < 9) {
+    str[*str_index] = mappings[mapping_index - 1];
+    (*str_index)++;
+    int i;
+    for(i = 0; i < num_times_into - 5; i++) {
+      str[*str_index] = mappings[mapping_index];
+      (*str_index)++;
+    }
+  }
+  else if(num_times_into == 9){
+    str[*str_index] = mappings[mapping_index];
+    (*str_index)++;
+    str[*str_index] = mappings[mapping_index - 2];
+    (*str_index)++;
+  }
+  *cur_total -= (num_times_into * places[cur_place]);
 }
+
 
 void run_test(char* result, char* expected) {
   if(!strcmp(result, expected)) {
@@ -111,9 +71,11 @@ int main(int argc, char* argv[]) {
   run_test(intToRoman(2000), "MM");
   run_test(intToRoman(3000), "MMM");
 
+
   run_test(intToRoman(900), "CM");
   run_test(intToRoman(1900), "MCM");
-  
+
+
   run_test(intToRoman(800), "DCCC");
   run_test(intToRoman(700), "DCC");
   run_test(intToRoman(600), "DC");
@@ -132,4 +94,5 @@ int main(int argc, char* argv[]) {
   run_test(intToRoman(50), "L");
 
   run_test(intToRoman(1), "I");
+
 }
